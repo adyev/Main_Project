363e4b9eda1bcd00c2b08e4bc74b63c1eed644b9
adyev <adyev.dm@yandex.ru>
2016-05-15 18:37:30 +0300

    qwerty

--- app/src/main/java/com/example/circles/Dots.java
+++ app/src/main/java/com/example/circles/Dots.java
@@ -12,10 +12,14 @@ public class Dots {
     float y;
     Paint paint;
     float radius;
-    Dots(float x, float y, float radius, Paint paint){
+    float number;
+    boolean exist;
+    Dots(float x, float y, float radius, Paint paint, float number, boolean exist){
         this.x=x;
         this.y=y;
         this.radius = radius;
         this.paint = paint;
+        this.number = number;
+        this.exist = exist;
     }
 }
